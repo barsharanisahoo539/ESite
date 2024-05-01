8308bde06a6f8d2512eb25a192745dafcb058949
barsh <barsh@BARSHARANI>
2024-05-01 13:13:54 +0530

    Add project files.

---
+++ ESite.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ESite.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESite.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ESite.Web/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESite.Web.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CeatedDateTime { get; set; } = DateTime.Now;
+    }
+}
